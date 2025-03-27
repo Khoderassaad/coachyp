b067380d9b7e6d2785fc4c954a4905ca600c7c7c
khoder <khoderassaad77@gmail.com>
2025-03-27 21:13:02 +0200

    hh

--- lib/features/Posts/presentation/pages/HomePage.dart
+++ lib/features/Posts/presentation/pages/HomePage.dart
@@ -15,7 +15,11 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: null,
       backgroundColor: AppColors.primary,
-      body: Padding(
+      body: 
+      
+      
+      
+      Padding(
         padding: EdgeInsets.only(top: 50),
         child: Column(
           
@@ -66,13 +70,98 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await FirebaseAuth.instance.signOut();
-            Navigator.of(context)
-                .pushNamedAndRemoveUntil("Login", (Route) => false);
+      // floatingActionButton: FloatingActionButton(
+      //     onPressed: () async {
+      //       await FirebaseAuth.instance.signOut();
+      //       Navigator.of(context)
+      //           .pushNamedAndRemoveUntil("Login", (Route) => false);
+      //     },
+      //     child: Icon(Icons.exit_to_app_rounded)),
+      
+    @override
+  Widget build(BuildContext context) {
+    /// [AnnotatedRegion<SystemUiOverlayStyle>] only for android black navigation bar. 3 button navigation control (legacy)
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        systemNavigationBarColor: navigationBarColor,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+      child: Scaffold(
+        // backgroundColor: Colors.grey,
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: pageController,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.bookmark_rounded,
+                size: 56,
+                color: Colors.amber[400],
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.favorite_rounded,
+                size: 56,
+                color: Colors.red[400],
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.email_rounded,
+                size: 56,
+                color: Colors.green[400],
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.folder_rounded,
+                size: 56,
+                color: Colors.blue[400],
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: WaterDropNavBar(
+          backgroundColor: navigationBarColor,
+          onItemSelected: (int index) {
+            setState(() {
+              selectedIndex = index;
+            });
+            pageController.animateToPage(selectedIndex,
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.easeOutQuad);
           },
-          child: Icon(Icons.exit_to_app_rounded)),
+          selectedIndex: selectedIndex,
+          barItems: <BarItem>[
+            BarItem(
+              filledIcon: Icons.bookmark_rounded,
+              outlinedIcon: Icons.bookmark_border_rounded,
+            ),
+            BarItem(
+                filledIcon: Icons.favorite_rounded,
+                outlinedIcon: Icons.favorite_border_rounded),
+            BarItem(
+              filledIcon: Icons.email_rounded,
+              outlinedIcon: Icons.email_outlined,
+            ),
+            BarItem(
+              filledIcon: Icons.folder_rounded,
+              outlinedIcon: Icons.folder_outlined,
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+      
+    );
+    
   }
 }
