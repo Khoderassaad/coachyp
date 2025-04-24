6ca8c2f8834ce9b1381be850bbfd47665e5eef6d
khoder <khoderassaad77@gmail.com>
2025-04-24 21:33:06 +0300

    merge ghlt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:coachyp/Pages/HomePage.dart';
-import 'package:coachyp/Pages/firebase_notifications.dart';
 import 'package:coachyp/Stripe_Payment/Stripe_keys.dart';
 import 'package:coachyp/features/auth/presentation/pages/login.dart';
-import 'package:coachyp/features/auth/presentation/pages/sign_up.dart';
+// import 'package:coachyp/features/auth/presentation/pages/User_sign_up.dart';
 import 'package:coachyp/features/auth/presentation/pages/welcome.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -10,17 +9,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'firebase/firebase_options.dart';
 
+
+
 void main() async {
   Stripe.publishableKey=ApiKeys.publishableKey;
   
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+  await Firebase.initializeApp( 
     options: DefaultFirebaseOptions.currentPlatform,
   );
-await FirebaseNotifications().initNotifications();
   runApp(COACHY());
 }
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class COACHY extends StatefulWidget {
   const COACHY({super.key});
@@ -46,11 +45,11 @@ class _COACHYState extends State<COACHY> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FutureBuilder(
+      home:  FutureBuilder(
         future: Future.delayed(const Duration(seconds: 2)), // Splash duration
         builder: (context, snapshot) {
           if (snapshot.connectionState != ConnectionState.done) {
-            return const HomePage(); // Show splash screen
+            return const Welcome(); // Show splash screen
           }
           
           // Check auth state after splash
@@ -63,10 +62,13 @@ class _COACHYState extends State<COACHY> {
         },
       ),
       routes: {
-        "sign_up": (context) => const sign_up(),
+        // "sign_up": (context) => const ClientSignUp(),
         "HomePage": (context) => const HomePage(),
         "Login": (context) => const Login(),
+        
+
       },
+      
     );
   }
 }
